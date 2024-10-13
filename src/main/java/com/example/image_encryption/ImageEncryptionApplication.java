2b3af637af4141e520fc1cc4da5042e3a6d8e06d
Yash Thakare <thakareyash74@gmail.com>
2024-10-13 11:15:10 +0530

    Initial commit

---
+++ src/main/java/com/example/image_encryption/ImageEncryptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.image_encryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageEncryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageEncryptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/image_encryption/ImageEncryptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.image_encryption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageEncryptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
